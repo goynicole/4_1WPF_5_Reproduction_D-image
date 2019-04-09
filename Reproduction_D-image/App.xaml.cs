4b51c38e1ddb2c53e8a17752b107a835166898a7
goy <goy.nicole018@gmail.com>
2019-04-09 12:59:08 +0200

    Découverte du WPF

---
+++ Reproduction_D-image/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Reproduction_D_image
+{
+    /// <summary>
+    /// Logique d'interaction pour App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
